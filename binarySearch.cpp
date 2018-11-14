866cd838d808f9cc5de7b9c10c79ded928d74aa5
Quamiemma <42769719+Quamiemma@users.noreply.github.com>
2018-11-14 07:31:55 +0000

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,19 +2,49 @@
 
 using namespace std;
 
+
+int binarySearch(const int list[], int listLength, int searchItem);
 int main (){
-  int arr[] = {21, 12, 5, 3, 8};
+  int array[] = {21, 12, 5, 3, 8};
 	int temp = 0;
-	
+	//loop to sort the array in ascending order
 	for (int i=0; i<5; i++){
 		for (int j=0; j<4; j++){
-			if (arr[j] > arr[j+1]){
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
+			if (array[j] > array[j+1]){
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
 			}
 		}
 	}
+	
+	//loop to print the sorted array
+	for (int j = 0; j<5; j++){
+		cout << array[j] << " ";
+		
+	}
+	cout << endl;
 
   return 0;
+}
+
+int binarySearch(const int list[], int listLength, int searchItem) { 
+	int first = 0; int last = listLength - 1; 
+	int mid;
+	bool found = false;
+	
+	while (first <= last && !found) { 
+		mid = (first + last) / 2;
+		if (list[mid] == searchItem) 
+			found = true; 
+		else if (list[mid] > searchItem) 
+			last = mid - 1; 
+		else 
+			first = mid + 1;
+	}
+	if (found) 
+		return mid; 
+	else {
+		return -1;
+	} 
 }
